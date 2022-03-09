9f0753608cdf52df4d8bb9df52928d4963e38bab
Krane96 <69842760+Krane96@users.noreply.github.com>
2022-03-09 13:00:56 +0100

    test

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
     url: env('https://hotelstrapi.herokuapp.com/'),
     proxy: true,
-    app: {keys: env.array('APP_KEYS',
-     ['hotel-key',
-      'hotel-secret']),
+    app: {
+        keys: env.array('APP_KEYS', ['hotel-key', 'hotel-secret']),
       },});
